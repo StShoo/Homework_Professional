a4a69064ae8188feb6fa8b42c13362044f89a602
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-16 20:36:19 +0200

    finished task 1

---
+++ Homework_P_1_task_1/MonthData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_P_1_task_1
+{
+    internal class MonthData
+    {
+        public string MonthName { get; set; }
+        public int MonthDays { get; set; }
+        public MonthData(string monthName, int monthDays)
+        {
+            MonthName = monthName;
+            MonthDays = monthDays;
+        }
+    }
+}
