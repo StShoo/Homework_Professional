48780f25c68708c835e2f95c1e27f8f05de90810
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-16 20:41:35 +0200

    finished task 2

---
+++ Homework_P_1_task_2/HumanData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_P_1_task_2
+{
+    internal class HumanData
+    {
+        public string HumanName { get; set; }
+        public int HumanAge { get; set; }
+        public HumanData(string name, int age)
+        {
+            HumanName = name;
+            HumanAge = age;
+        }
+    }
+}
