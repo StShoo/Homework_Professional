cec073e6ddb3d018cbdf26a831156ad3d6d96ff5
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-18 19:55:54 +0200

    finished task 1

---
+++ Homework_P_3_task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Homework_P_3_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating file...");
+            var file = new FileInfo("Text.txt");
+
+            StreamWriter writer = file.CreateText();
+            writer.WriteLine("Some information");
+            writer.Close();
+
+            Console.WriteLine($"{file.Name} was created");
+
+            Console.WriteLine($"{file.Name} contains: ");
+            StreamReader reader = file.OpenText();
+            Console.WriteLine(reader.ReadToEnd());
+            reader.Close();
+        }
+    }
+}
