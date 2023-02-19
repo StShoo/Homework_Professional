681e61cefd4e632b6f1d3a61d49c1073cd1cd759
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-19 09:58:11 +0200

    finished task 4

---
+++ Homework_P_3_task_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace Homework_P_3_task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 100; i++) 
+            {
+                string filename = "Folder_" + i;
+                var file = new FileInfo(filename);
+                FileStream stream = file.Create();
+                stream.Close();
+            }
+
+            Console.WriteLine("Files created");
+            Console.ReadKey();
+
+            for (int i = 0; i < 100; i++)
+            {
+                string filename = "Folder_" + i;
+                var file = new FileInfo(filename);
+                file.Delete();
+            }
+        }
+    }
+}
