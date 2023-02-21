7b68f92e3cc6d63543fc9a1862cc7a053a2f814e
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-21 18:31:27 +0200

    finished task 3

---
+++ Homework_P_4_task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Homework_P_4_task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegionInfo regionInfo = RegionInfo.CurrentRegion;
+            CultureInfo newCulture = CultureInfo.CurrentCulture;
+
+            StreamReader streamReader = new StreamReader("recipt.txt");
+            string fileText = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            Console.WriteLine("Current local:");
+            Console.WriteLine(Regex.Replace(fileText, @"грн", regionInfo.CurrencyNativeName));
+
+            Console.WriteLine(new string('-', 20));
+
+            regionInfo = new RegionInfo("en-US");
+            newCulture = CultureInfo.GetCultureInfo("en-US");
+
+            Console.WriteLine("USA local:");
+            Console.WriteLine(Regex.Replace(fileText, @"грн", regionInfo.CurrencyNativeName));
+        }
+    }
+}
