e0b34ea9ca0b7489ba0173bbf6cb6c77c4e31b66
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-21 18:59:54 +0200

    finished task 2

---
+++ Homework_P_4_task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Homework_P_4_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader("text.txt");
+
+            string fileText = streamReader.ReadToEnd();
+            streamReader.Close();
+
+
+            fileText = Regex.Replace(fileText, @"пiд", "ГАВ");
+            fileText = Regex.Replace(fileText, @"вiд", "ГАВ");
+            fileText = Regex.Replace(fileText, @"бiля", "ГАВ");
+            Console.WriteLine(fileText);
+
+            StreamWriter streamWriter = new StreamWriter("text.txt");
+            streamWriter.Write(fileText);
+            streamWriter.Close();
+        }
+    }
+}
