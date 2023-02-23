bb7ab2e152d3acafbaa459c48b19d522e832d366
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-23 21:01:39 +0200

    finished task 2

---
+++ Homework_P_6_task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using TemperatureConverter;
+
+namespace Homework_P_6_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureConvert tempConverter = new TemperatureConvert(20.77, 'F');
+            tempConverter.ConvertTemperature();
+        }
+    }
+}
---
+++ TemperatureConverter/TemperatureConvert.cs
@@ -0,0 +1,27 @@
+﻿namespace TemperatureConverter
+{
+    public class TemperatureConvert
+    {
+        private double temperature;
+        private char temperatureSign;
+
+        public TemperatureConvert(double temperature, char temperatureSign)
+        {
+            this.temperature = temperature;
+            this.temperatureSign = temperatureSign;
+        }
+
+        public void ConvertTemperature()
+        {
+            if (temperatureSign.Equals('C'))
+            {
+                Console.WriteLine($"{temperature} {temperatureSign} = {temperature * 1.8 + 32} F");
+            }
+            else if (temperatureSign.Equals('F'))
+            {
+                Console.WriteLine($"{temperature} {temperatureSign} = {(temperature - 32)/1.8} C");
+            }
+            else { Console.WriteLine("data is incorect"); }
+        }
+    }
+}
