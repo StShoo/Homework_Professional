9a7cdd129df079b9f17c94dd2817c8379977ad39
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-25 18:47:55 +0200

    finished task 2

---
+++ Homework_P_7_task_2/AttributeClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_P_7_task_2
+{
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    class MyAttribute: Attribute
+    {
+        private readonly string name;
+        private int num;
+
+        public MyAttribute(string name)
+        {
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public int Num
+        {
+            get { return num; }
+            set { num = value; }
+        }
+    }
+}
