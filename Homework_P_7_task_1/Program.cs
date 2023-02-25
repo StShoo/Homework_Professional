f4fb3a5f71ceb49dcae4dad83dab1c570666cb95
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-25 19:00:19 +0200

    finished task 1

--- Homework_P_7_task_1/Program.cs
+++ Homework_P_7_task_1/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Homework_P_7_task_1
+﻿#define TRIAL
+//#define PREMIUM
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace Homework_P_7_task_1
 {
-    internal class Program
+    
+    class MyClass
     {
-        static void Main(string[] args)
+        [Obsolete("We do not need this function anymore", false)]
+        public void SayHallo()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hallo");
+        }
+        [Obsolete("Do not use it!", true)]
+        public void SayBye()
+        {
+            Console.WriteLine("Bye");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass myClass = new MyClass();
+            myClass.SayHallo();
+            myClass.SayBye();
         }
     }
 }
