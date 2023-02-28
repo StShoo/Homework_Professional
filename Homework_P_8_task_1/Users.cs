2add8c4cf4595b6c48ed5b8ad3d1284f96a085de
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-28 20:19:14 +0200

    finished task 1

---
+++ Homework_P_8_task_1/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Homework_P_8_task_1
+{
+    [XmlRoot("PlayersData")]
+    public class Users
+    {
+        private List<string> usersData = new List<string>();
+        [XmlArray("ListOfPlayers")]
+        [XmlArrayItem("Player")]
+        public List<string> UsersData
+        {
+            get { return usersData; }
+            set { usersData = value; }
+        }
+    }
+}
